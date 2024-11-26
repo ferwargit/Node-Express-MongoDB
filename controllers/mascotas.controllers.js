e351eb6101740d49f4b59d947aec0ede77a382ad
fernando <ferwar@hotmail.com>
2024-11-26 13:23:14 -0300

    refactoriza para mongoose en el modelo

--- controllers/mascotas.controllers.js
+++ controllers/mascotas.controllers.js
@@ -1,4 +1,4 @@
-import mascotasModel from "../models/mascotas.models.js";
+import mascotaModel from "../models/mascotas.models.js";
 
 class mascotasController {
     constructor() {
@@ -8,7 +8,7 @@ class mascotasController {
     async create(req, res) {
         try {
             // Lógica para crear una nueva mascota
-            const data = await mascotasModel.create(req.body);
+            const data = await mascotaModel.create(req.body);
             res.status(201).json(data);
         } catch (e) {
             res.status(500).send(e); // Manejo de errores
@@ -19,7 +19,7 @@ class mascotasController {
         try {
             // Lógica para crear una nueva mascota
             const {id} = req.params;
-            const data = await mascotasModel.update(id, req.body);
+            const data = await mascotaModel.update(id, req.body);
             res.status(200).json(data);
         } catch (e) {
             res.status(500).send(e); // Manejo de errores
@@ -30,7 +30,7 @@ class mascotasController {
         try {
             // Lógica para crear una nueva mascota
             const {id} = req.params;
-            const data = await mascotasModel.delete(id);
+            const data = await mascotaModel.delete(id);
             res.status(206).json(data);
         } catch (e) {
             res.status(500).send(e); // Manejo de errores
@@ -40,7 +40,7 @@ class mascotasController {
     async getAll(req, res) {
         try {
             // Lógica para obtener todas las mascotas
-            const data = await mascotasModel.getAll();
+            const data = await mascotaModel.getAll();
             res.status(201).json(data); // Respuesta de éxito
         } catch (e) {
             res.status(500).send(e); // Manejo de errores
@@ -51,7 +51,7 @@ class mascotasController {
         try {
             // Lógica para obtener una mascota por ID
             const {id} = req.params;
-            const data = await mascotasModel.getOne(id);
+            const data = await mascotaModel.getOne(id);
             res.status(201).json(data); // Respuesta de éxito
         } catch (e) {
             res.status(500).send(e); // Manejo de errores
--- models/mascotas.models.js
+++ models/mascotas.models.js
@@ -1,33 +1,28 @@
-import dbClient from "../config/dbClient.js";
-import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+import Mascota from "../schemas/mascotas.schemas.js";
 
-class mascotasModel {
+class mascotaModel {
 
     async create(mascota) {
-        const colMascotas = dbClient.db.collection("mascotas");
-        return await colMascotas.insertOne(mascota);
+        return await Mascota.create(mascota);
     }
 
     async update(id, mascota) {
-        const colMascotas = dbClient.db.collection("mascotas");
-        return await colMascotas.updateOne({ _id: new ObjectId(id) }, { $set: mascota });
+        return await Mascota.findOneAndUpdate({ _id: new mongoose.Types.ObjectId(id) }, mascota, { new: true });
     }
 
     async delete(id) {
-        const colMascotas = dbClient.db.collection("mascotas");
-        return await colMascotas.deleteOne({ _id: new ObjectId(id) });
+        return await Mascota.findOneAndDelete({ _id: new mongoose.Types.ObjectId(id) });
     }
 
     async getAll() {
-        const colMascotas = dbClient.db.collection("mascotas");
-        return await colMascotas.find({}).toArray();
+        return await Mascota.find();
     }
 
     async getOne(id) {
-        const colMascotas = dbClient.db.collection("mascotas");
-        return await colMascotas.findOne({ _id: new ObjectId(id) });
+        return await Mascota.findById(id);
     }
 
 }
 
-export default new mascotasModel();
+export default new mascotaModel();
