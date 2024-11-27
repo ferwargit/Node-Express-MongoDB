8b447be7c6273a483988843ae1d539261108e4ba
fernando <ferwar@hotmail.com>
2024-11-26 23:55:16 -0300

    ruta /profile

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import usuariosController from '../controllers/usuarios.controllers.js';
+import { verificarToken } from '../helpers/autenticacion.js';
 
 const route = express.Router();
 
 // Crear una nuevo usuario
 route.post('/register', usuariosController.register);
 route.post('/login', usuariosController.login);
+route.get('/profile', verificarToken, usuariosController.profile);
 
 export default route;
