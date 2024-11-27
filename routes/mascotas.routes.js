d1fe742d801dde42c43dbea5774df1035e04fe25
fernando <ferwar@hotmail.com>
2024-11-26 23:54:51 -0300

    se añade el middleware verificarToken

--- routes/mascotas.routes.js
+++ routes/mascotas.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mascotasController from '../controllers/mascotas.controllers.js'; // Importar el controlador
+import { verificarToken } from '../helpers/autenticacion.js';
 
 const route = express.Router(); // Crear un nuevo enrutador
 
@@ -10,8 +11,10 @@ route.get('/:id', mascotasController.getOne);
 // Obtener todas las mascotas
 route.get('/', mascotasController.getAll);
 // Actualizar una mascota por su ID
-route.put('/:id', mascotasController.update);
+route.put('/:id', verificarToken, mascotasController.update);
+// route.put('/:id', mascotasController.update);
 // Eliminar una mascota por su ID
-route.delete('/:id', mascotasController.delete);
+route.delete('/:id', verificarToken, mascotasController.delete);
+// route.delete('/:id', mascotasController.delete);
 
 export default route; // Exportar el enrutador
